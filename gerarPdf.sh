cd1abac73d8c2d19c8a946d604ed1de00c920142
Rafael Beserra <rafaelufrn@gmail.com>
2021-04-12 18:18:11 -0300

    Update gerarPdf.sh

--- gerarPdf.sh
+++ gerarPdf.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
-#create a output folder, compile a .tex file using pdflatex and output as folder for temporary files
-#and then open the pdf using evince
+#if it is a .tex file:
+#	creates an output folder, compiles the .tex file using pdflatex and output as folder for temporary files
+#	and then opens the pdf using evince
+#if it is a .md file:
+#	compiles it to pdf using pandoc using a small margin
 
 if [ -z "$1" ]; then
-	echo "Missing argument. Dont use .tex extension. If the file is baseName.tex, use gerarPdf baseName"
+	echo "Missing argument."
 else
-	if [ ! -d output ]; then
-		mkdir output
-	fi
-	filename=$1
-	if [ -f $filename.tex ] ; then
-		pdflatex -output-directory=./output/ $filename.tex
-		evince ./output/$filename.pdf &
+	ext="${1#*.}"
+	if [[ "$ext" == "md" ]]; then
+		pandoc $1 -o ${1%.md}.pdf -V geometry:margin=1in
+		evince ${1%.md}.pdf &
 	else
-		pdflatex -output-directory=./output/ $filename
-		evince ./output/${filename%.tex}.pdf &
+		if [ ! -d output ]; then
+			mkdir output
+		fi
+		filename=$1
+		if [ -f $filename.tex ] ; then
+			pdflatex -output-directory=./output/ $filename.tex
+			evince ./output/$filename.pdf &
+		else
+			pdflatex -output-directory=./output/ $filename
+			evince ./output/${filename%.tex}.pdf &
+		fi
 	fi
 fi
